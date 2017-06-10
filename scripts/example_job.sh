ef96207812787fefa430f8868792208db6a20ca5
Philipp Paris <paris.philipp@gmail.com>
2017-06-10 19:50:51 +0200

    fixed example_job bash script

--- scripts/example_job.sh
+++ scripts/example_job.sh
@@ -6,12 +6,11 @@
 #SBATCH --job-name=scatter_32x16
 #SBATCH --ntasks-per-node=16
 
-srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/$
-srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/$
-srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/$
-srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/$
-srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/$
-
+srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/scatter_out1.dat
+srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/scatter_out2.dat
+srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/scatter_out3.dat
+srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/scatter_out4.dat
+srun ~/hpc17/bin/mpibenchmark --input-file=/home/students/e1325664/hpc17/scripts/input_scatter.txt > /home/students/e1325664/output/scatter_out5.dat
 
 
 
